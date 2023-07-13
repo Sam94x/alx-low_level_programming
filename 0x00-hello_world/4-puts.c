1cfb9f1cec783ed8a355c083bf83cc12b2b790b2
Oussama <oussama94ichou@gmail.com>
2023-07-13 16:51:17 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - the main function
+*
+*
+*Return: return 0 everytime
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
