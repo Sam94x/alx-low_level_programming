9833ebd7dfe26ac9e9e70e6ad30321dcdc234ae7
Oussama <oussama94ichou@gmail.com>
2023-07-18 11:17:19 -0700

    done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_to_98 - prints natural
+*numbers from n to 98
+*@n:integer to start by
+*return: returns nothing
+*/
+
+void print_to_98(int n)
+{
+	int c;
+
+	if (n > 98)
+		for (c = n; c > 98; c--)
+			printf("%d, ", c);
+	else
+		for (c = n; c < 98; c++)
+			printf("%d, ", c);
+	printf("98\n");
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(48);
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar('.');
+			_putchar(' ');
+
+			k = i * j;
+			if (k <= 9)
+				_putchar('');
+			else
+				_putchar((k / 10) + 48);
+			_putchar((k % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
