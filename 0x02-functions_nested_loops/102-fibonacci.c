0fb80fcfb20b078354b340c97ea233f38f85fe93
Oussama <oussama94ichou@gmail.com>
2023-07-19 15:33:37 -0700

    done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - prints out first 50
+*fibonacci suit numbers
+*Return: return 0
+*/
+
+int main(void)
+{
+	int c;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (c = 0; c < 50; c++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (c == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
