7f54dd717e374af9fd07c9796fdcde0cfd7b9f3c
Oussama <oussama94ichou@gmail.com>
2023-07-26 15:12:02 -0700

    done

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,15 +6,15 @@
 *Return: returns a character
 */
 
-char *string_toupper(char *n)
+char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (n[i] != '\0')
+	while (str[i])
 	{
-		if (n[i] >= 'a' && n[i] <= 'z')
-			n[i] = n[i] - 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
 		i++;
 	}
-	return (n);
+	return (str);
 }
