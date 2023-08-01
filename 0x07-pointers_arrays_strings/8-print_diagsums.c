7282f8dc6a6251452c4561c8ea423bbe66d061d4
Oussama <oussama94ichou@gmail.com>
2023-07-31 17:55:13 -0700

    done

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,26 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_diagsums - Entry point
- * @a: input
- * @size: input
- * Return: Always 0 (Success)
+ * print_diagsums -  prints sums
+ * @a: a pointer
+ * @size: size
  */
 void print_diagsums(int *a, int size)
 {
-	int i, s1 = 0, s2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		s1 += a[i];
-		a += size;
+		sum1 = sum1 + a[y * size + i];
 	}
-	a -= size;
 
-	for (i = 0; i < size; i++)
+	for (i = size - 1; i >= 0; i--)
 	{
-		s2 += a[i];
-		a -= size;
+		sum2 += a[i * size + (size - i - 1)];
 	}
-	printf("%d, %d\n", sum1, sum2);
+
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
 }
